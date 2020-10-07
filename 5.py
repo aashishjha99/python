d62b4b9fb7e87bee46a9dcd2934f13b02221fae3
AASHISH <aashishjhaoff@gmail.com>
2020-10-07 11:59:46 +0530

    5

---
+++ 5.py
@@ -0,0 +1,15 @@
+names = [9.9, 'ash','jha']
+nums = [44,56,78]
+mil = [nums,names]
+# print(mil)
+nums.remove(56)
+print(nums)
+
+print(nums.pop())
+nums.extend([29,12,56,77,89])
+print(nums)
+print(max(nums))
+print(sum(nums))
+nums.sort()
+print(nums)
+
---
+++ strings.py
@@ -0,0 +1,4 @@
+a = "  Hello,  World!"
+print(a.strip())
+print(a.lower())
+print(a.upper())
