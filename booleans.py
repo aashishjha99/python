5c14b65efba10db024c3c7f573d10545315edbd7
AASHISH <aashishjhaoff@gmail.com>
2020-10-07 17:45:27 +0530

    boolean

---
+++ booleans.py
@@ -0,0 +1,17 @@
+print(10 > 9)
+print(10==9)
+print(10<9)
+
+a = 200
+b = 33
+
+if b>a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+print(bool("hello"))
+print(bool(15))
+
+x = 20
+print(isinstance(x,int))
