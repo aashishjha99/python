90b03d5214810133eea4eff87b880afea5afa998
AASHISH <aashishjhaoff@gmail.com>
2020-10-08 22:34:05 +0530

    tuples

---
+++ tuples.py
@@ -0,0 +1,11 @@
+#thistuple = ("apple","banana","orange","kiwi","cheery","melon")
+#y = list(thistuple)
+#y[1] = "lichi"
+#thistuple = tuple(y)
+#print(thistuple)
+
+#for z in thistuple:
+#    print(z)
+thistuple = (1,3,4,5,6,7)
+x = thistuple.index(5)
+print(x)
