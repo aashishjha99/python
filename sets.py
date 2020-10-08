bf4409f8a5a184e7b60a940f48a07d2a93f26322
AASHISH <aashishjhaoff@gmail.com>
2020-10-08 22:55:18 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,9 @@
+thisset = {"apple","banana","cherry"}
+#print(thisset)
+#for x in thisset:
+ #   print(x)
+#print("banana" in thisset)
+thisset.update(["orange","angur","aamras"])
+#print(len(thisset))
+thisset.remove("angur")
+print(thisset)
