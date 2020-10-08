f3f402e52c384c77171572e9c902d46e5db7e609
AASHISH <aashishjhaoff@gmail.com>
2020-10-08 12:29:56 +0530

    logical

---
+++ logical.py
@@ -0,0 +1,12 @@
+x = 12
+print(x > 6 and x < 10)
+
+x = ["banana","apple"]
+y = ["banana","apple"]
+z = x
+print(x is not z)
+print(x is  not y)
+print(x  != y)
+
+x = ["banana","apple"]
+print("aam"  not in x)
