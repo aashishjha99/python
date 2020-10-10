02bfa2c27cbee19911517283562a0839d91aa7a4
AASHISH <aashishjhaoff@gmail.com>
2020-10-10 10:10:33 +0530

    sets

--- sets.py
+++ sets.py
@@ -1,9 +1,20 @@
-thisset = {"apple","banana","cherry"}
+#thisset = {"apple","banana","cherry"}
 #print(thisset)
 #for x in thisset:
  #   print(x)
 #print("banana" in thisset)
-thisset.update(["orange","angur","aamras"])
+#thisset.update(["orange","angur","aamras"])
 #print(len(thisset))
-thisset.remove("angur")
+#thisset.remove("angur") #throws errors if it not exist
+#print(thisset)
+#thisset.discard("snake") #dont throw error , if it does't exist 
+#thisset.clear()
+#z = thisset.pop()
+#del #thisset #it will givw error not exist
+
+#set1 = {"a","b","c"}
+#set2 = {1,2,3}
+#set3 = set1.union(set2) #returns new set containing both sets
+#set1.update(set2)
+thisset = set(("apple","banana","cherry"))
 print(thisset)
